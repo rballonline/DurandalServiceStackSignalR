71b3b6cd4179b84abb31ef60334abd8bb082ec39
Will T <rballonline@gmail.com>
2013-12-20 14:35:53 -0800

    Cleaning up and adding projects

--- DurandalServiceStackSignalR/Global.asax.cs
+++ DurandalServiceStackSignalR/Global.asax.cs
@@ -18,11 +18,9 @@ namespace DurandalServiceStackSignalR
 		{
 			AreaRegistration.RegisterAllAreas();
 
-			WebApiConfig.Register(GlobalConfiguration.Configuration);
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
-			AuthConfig.RegisterAuth();
 		}
 	}
 }
